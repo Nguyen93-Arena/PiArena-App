3ab4ad81eccf26c8557edab0a8bac794bb1e0af2
Nguyen93-Arena <vannguyenbp230393@gmail.com>
2025-07-01 11:55:13 +0700

    Create document.js

---
+++ pages/document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Nhúng Pi SDK */}
+          <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
